0dff5392eead6b0fab259c85f45a218979d514bb
lichengcai <lichengcai@koolearn.com>
2016-09-30 11:13:43 +0800

    集成butterknife

--- app/src/main/java/com/snail/ui/activity/ActivitySplash.java
+++ app/src/main/java/com/snail/ui/activity/ActivitySplash.java
@@ -1,10 +1,22 @@
 package com.snail.ui.activity;
 
-import android.app.Activity;
+import android.os.Bundle;
+
+import com.snail.R;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by lichengcai on 2016/9/29.
  */
 
-public class ActivitySplash extends Activity {
+public class ActivitySplash extends ActivityBase {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+    }
+
+
 }
