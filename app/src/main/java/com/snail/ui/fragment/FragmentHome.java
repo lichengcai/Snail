6930398fd0c7fa3899e9e6d8b6bcdbf19fe10a1b
zhangshibiao <zhangshibiao@jiedaibao.com>
2016-10-08 21:54:43 +0800

    .

--- app/src/main/java/com/snail/ui/fragment/FragmentHome.java
+++ app/src/main/java/com/snail/ui/fragment/FragmentHome.java
@@ -26,7 +26,6 @@ public class FragmentHome extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
-//        view.findViewById();
         initGridView(view);
         return view;
     }
