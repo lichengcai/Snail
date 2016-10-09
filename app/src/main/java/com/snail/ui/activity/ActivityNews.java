edea2245a64a4e8c805740333fa46fe6fea6db7d
lichengcai <974355195@qq.com>
2016-10-09 22:08:25 +0800

    ActivityNews

---
+++ app/src/main/java/com/snail/ui/activity/ActivityNews.java
@@ -0,0 +1,15 @@
+package com.snail.ui.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by chengcai on 2016/10/9.
+ */
+
+public class ActivityNews extends ActivityBase {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
