19f0aa9d04c2644d26912774b2183b5422e2bdcc
zhangshibiao <zhangshibiao@jiedaibao.com>
2016-10-09 22:57:42 +0800

    news activity

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 
 ## refrences
 - [ANDROID调用系统相机拍照](http://stormzhang.com/android/2013/10/19/android-call-camera/)
+
+
+
+## network
+- [learning](http://m.sohu.com/ca/?channel=learning&index=3)
---
+++ app/src/main/java/com/snail/ui/activity/NewsActivity.java
@@ -0,0 +1,15 @@
+package com.snail.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.snail.R;
+
+public class NewsActivity extends ActivityBase {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
